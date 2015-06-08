fa5e431a3cc09cb79db98a71b550024c77f08fc8
bujjibabu <bujji404@gmail.com>
2015-06-08 08:46:42 +0530

    some ui changes..

--- app/scripts/controllers/mystoryCtrl.js
+++ app/scripts/controllers/mystoryCtrl.js
@@ -8,11 +8,14 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-    .controller('MystoryCtrl', function() {
+    .controller('MystoryCtrl', function($scope,$http) {
 
-
-       
-
-
+       $http.get('data/mystory.json').
+	    success(function(data/*, status, headers, config*/) {
+	      $scope.mystoryData = data;
+	    }).
+	    error(function(/*data, status, headers, config*/) {
+	      console.log('unable to get data');
+	    });
 
     });
--- app/scripts/controllers/storyFeedCtrl.js
+++ app/scripts/controllers/storyFeedCtrl.js
@@ -8,6 +8,25 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('storyFeedCtrl', function () {
+  .controller('storyFeedCtrl', function ($scope, $http) {
+
+  	 $http.get('data/mystory.json').
+	    success(function(data/*, status, headers, config*/) {
+	      $scope.storyFeed  = data;
+
+	      var x = 12;
+	      var y = 34;
+	      var z = x+y;
+
+	      if(z>100){
+	      	alert('success')
+	      }else{
+	      	alert('fail');
+	      }
+
+	    }).
+	    error(function(/*data, status, headers, config*/) {
+	      console.log('unable to get data');
+	    });
     
   });
